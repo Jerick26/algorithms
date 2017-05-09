2446ef7861cfc78c01a630a94210489ad1bf04a7
Jerick26 <jerry.pin26@yahoo.com>
2017-05-09 21:59:13 +0800

    Update secondLargestNode.java

--- secondLargestNode.java
+++ secondLargestNode.java
@@ -6,3 +6,26 @@ if (rightmost.left != null) {
 else{
     return rightmost.parent
 }
+
+public static int findSecondLargestValueInBST(Node root)
+{
+  int secondMax;
+  Node pre = root;
+  Node cur = root;
+  while (cur.Right != null) {
+    pre = cur;
+    cur = cur.Right;
+  }
+  if (cur.Left != null) {
+    cur = cur.Left;
+    while (cur.Right != null)
+      cur = cur.Right;
+    secondMax = cur.Value;
+  } else {
+    if (cur == root && pre == root)
+      secondMax = int.MinValue;
+    else
+      secondMax = pre.Value;
+  }
+  return secondMax;
+}
