eaee32a4fdad58b15fa48d4fa1b19e5a846a00cf
Jerick26 <jerry.pin26@yahoo.com>
2017-05-09 21:48:24 +0800

    Update nextNode.java

--- nextNode.java
+++ nextNode.java
@@ -1,3 +1,42 @@
+// solution a
+public static TreeNode findSuccessor(TreeNode node)
+{
+  if (node == null)
+    return null;
+  
+  if (node.getRight() != null)
+    return findMinimum(node.getRight());
+  
+  TreeNode y = node.getParent();
+  TreeNode x = node;
+  while (y != null && x == y.getRight())
+  {
+    x = y;
+    y = y.getParent();
+  }
+  // Intuition: as we traverse left up the tree we traverse smaller values
+  // The first node on the right is the next larger number
+  return y;
+}
+
+// 
+public static TreeNode findPredecessor(TreeNode node)
+{
+  if (node == null)
+    return null;
+  if (node.getLeft() != null)
+    return findMaximum(node.getLeft());
+  TreeNode parent = node.getParent();
+  TreeNode y = parent;
+  TreeNode x = node;
+  while (y != null && x == y.getLeft()) {
+    x = y;
+    y = y.getParent();
+  }
+  return y;
+}
+
+// solution b
 Node nextNode(Node node) {
   if ( node == null ) { return null; }
   if ( node.right != null ) {
