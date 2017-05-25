36b259a82b331fe024ccea0b540f2e733f7064e3
Jerick26 <jerry.pin26@yahoo.com>
2017-05-26 00:18:23 +0800

    Update trieST.cpp
    run pass with get, put, keys methods

--- trieST.cpp
+++ trieST.cpp
@@ -7,20 +7,34 @@ using std::string;
 template <typename Value>
 class TrieST {
  public:
+  static const int R = 256; // radix
+ public:
   class Node {
    public:
-    Node() : val(NULL) {
+    Node() : val(nullptr) {
       for (int r=0; r<R; r++){
-        next[r] = NULL;
+        next[r] = nullptr;
       }
     }
+    ~Node() {
+      if (val != nullptr)
+        delete val;
+      for (int r=0; r<R; r++)
+        if (next[r] != nullptr)
+          delete next[r];
+    }
     Value* val;
-    Node* next[TrieST::R];
+    Node* next[R];
   };
+  TrieST() : root(nullptr) {}
+  ~TrieST() {
+    if (root != nullptr)
+      delete root;
+  }
   Value* get(const string& key) {
     Node* x = get(root, key, 0);
-    if (x == NULL)
-      return NULL;
+    if (x == nullptr)
+      return nullptr;
     return x->val;
   }
   void put(const string& key, Value* val) {
@@ -32,41 +46,41 @@ class TrieST {
   std::queue<string>* keysWithPrefix(const string& pre) {
     std::queue<string>* q = new std::queue<string>();
     collect(get(root, pre, 0), pre, q);
+    return q;
   }
  private:
   // return value associated with key in the subtrie rooted in x
   Node* get(Node* x, const string& key, int d) {
-    if (x == NULL)
-      return NULL;
+    if (x == nullptr)
+      return nullptr;
     if (d == key.length())
       return x;
     // use dth key char to identify subtrie
-    char c = key.at(d);
+    unsigned char c = key.at(d);
     return get(x->next[c], key, d+1);
   }
   // change value associated with key if in subtrie rooted in x
   Node* put(Node* x, const string& key, Value* val, int d) {
-    if (x == NULL)
+    if (x == nullptr)
       x = new Node();
     if (d == key.length()) {
       x->val = new Value(*val);
       return x;
     }
-    char c = key.at(d);
+    unsigned char c = key.at(d);
     x->next[c] = put(x->next[c], key, val, d+1);
     return x;
   }
   void collect(Node* x, const string& pre, std::queue<string>* q) {
-    if (x == NULL)
+    if (x == nullptr)
       return;
-    if (x->val != NULL)
+    if (x->val != nullptr)
       q->push(pre);
-    for (char c=0; c<R; c++)
-      collect(x->next[c], pre+c, q);
+    for (int c=0; c<R; c++)
+      collect(x->next[c], pre+(char)c, q);
   }
 
  private:
-  static const int R = 256; // radix
   Node* root;  // root of trie
 };
 
@@ -77,8 +91,10 @@ void test_get_put() {
   trie.put("a", &k1);
   trie.put("ab", &k2);
   trie.put("pi", &k3);
-  std::cout << "pi: " << trie.get("pi") << std::endl;
-  std::cout << "zh: " << trie.get("zh") << std::endl;
+  int* pi = trie.get("pi");
+  int* zh = trie.get("zh");
+  std::cout << "pi: " << (pi?*pi:-1) << std::endl;
+  std::cout << "zh: " << (zh?*zh:-1) << std::endl;
   std::cout << "test get put end" << std::endl;
 }
 
