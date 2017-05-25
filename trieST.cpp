53fccbe2921c5281bd0e25dd27eca76266b1af0d
Jerick26 <jerry.pin26@yahoo.com>
2017-05-25 21:51:02 +0800

    Create trieST.cpp
    note: x->val = new Value(*val);  // have coredump

---
+++ trieST.cpp
@@ -0,0 +1,113 @@
+#include <queue>
+#include <string>
+#include <iostream>
+
+using std::string;
+
+template <typename Value>
+class TrieST {
+ public:
+  class Node {
+   public:
+    Node() : val(NULL) {
+      for (int r=0; r<R; r++){
+        next[r] = NULL;
+      }
+    }
+    Value* val;
+    Node* next[TrieST::R];
+  };
+  Value* get(const string& key) {
+    Node* x = get(root, key, 0);
+    if (x == NULL)
+      return NULL;
+    return x->val;
+  }
+  void put(const string& key, Value* val) {
+    root = put(root, key, val, 0);
+  }
+  std::queue<string>* keys() {
+    return keysWithPrefix("");
+  }
+  std::queue<string>* keysWithPrefix(const string& pre) {
+    std::queue<string>* q = new std::queue<string>();
+    collect(get(root, pre, 0), pre, q);
+  }
+ private:
+  // return value associated with key in the subtrie rooted in x
+  Node* get(Node* x, const string& key, int d) {
+    if (x == NULL)
+      return NULL;
+    if (d == key.length())
+      return x;
+    // use dth key char to identify subtrie
+    char c = key.at(d);
+    return get(x->next[c], key, d+1);
+  }
+  // change value associated with key if in subtrie rooted in x
+  Node* put(Node* x, const string& key, Value* val, int d) {
+    if (x == NULL)
+      x = new Node();
+    if (d == key.length()) {
+      x->val = new Value(*val);
+      return x;
+    }
+    char c = key.at(d);
+    x->next[c] = put(x->next[c], key, val, d+1);
+    return x;
+  }
+  void collect(Node* x, const string& pre, std::queue<string>* q) {
+    if (x == NULL)
+      return;
+    if (x->val != NULL)
+      q->push(pre);
+    for (char c=0; c<R; c++)
+      collect(x->next[c], pre+c, q);
+  }
+
+ private:
+  static const int R = 256; // radix
+  Node* root;  // root of trie
+};
+
+void test_get_put() {
+  std::cout << "test get put begin ..." << std::endl;
+  TrieST<int> trie;
+  int k1 = 1, k2 = 2, k3 = 3;
+  trie.put("a", &k1);
+  trie.put("ab", &k2);
+  trie.put("pi", &k3);
+  std::cout << "pi: " << trie.get("pi") << std::endl;
+  std::cout << "zh: " << trie.get("zh") << std::endl;
+  std::cout << "test get put end" << std::endl;
+}
+
+void test_keys() {
+  std::cout << "test keys begin ..." << std::endl;
+  TrieST<int> trie;
+  int k1 = 1, k2 = 2, k3 = 3;
+  trie.put("a", &k1);
+  trie.put("ab", &k2);
+  trie.put("pi", &k3);
+  std::cout << "test keys" << std::endl;
+  std::queue<string>* q = trie.keys();
+  while (!q->empty()) {
+    std::cout << q->front() << std::endl;
+    q->pop();
+  }
+  delete q;
+  std::cout << "test keysWithPrefix" << std::endl;
+  std::queue<string>* p = trie.keysWithPrefix("a");
+  while (!p->empty()) {
+    std::cout << p->front() << std::endl;
+    p->pop();
+  }
+  delete p;
+  std::cout << "test keys end" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  test_get_put();
+  test_keys();
+  return 0;
+}
