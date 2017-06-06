78efcc6fcb497c8401ad3dd4ebf948482496439b
Jerick26 <jerry.pin26@yahoo.com>
2017-06-06 21:24:57 +0800

    Update word_freq.cpp

--- word_freq.cpp
+++ word_freq.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- *  Compilation:  g++ -o main main.cpp -std=c++0x -g
+ *  Compilation:  g++ -o main.cpp -std=c++0x -g
  *  Execution:    ./main n < input.txt
  *  Dependencies: std::cin std::cout
  *  Data files:   https://github.com/Jerick26/algorithms/blob/master/word_freq_test.data
@@ -8,80 +8,89 @@
  *    - parse a string to get the frequence information
  *    - get top N frequence words
  *
- *  % ./main 3 < word_freq_test.data
+ *  % ./a.out 3 < word_freq_test.data
  *  the top 3 words is:
  *  nob: 6
  *  joy: 5
  *  ilk: 4
+ *  tag: 4
+ *  wee: 4
  *
  ******************************************************************************/
 #include <string>
 #include <map>
-#include <vector>
+#include <list>
 #include <iostream>
-#include <climits>
-#include <cstdlib>
 
 using std::string;
 
-void split_to_words(const string& s, std::map<string, int>* w) {
-  int b = 0, e = 0;
-  int len = s.length();
-  while (b < len) {
-    while (b < len && s.at(b) == ' ')
-      b++;
-    e = b + 1;
-    while (e < len && s.at(e) != ' ')
-      e++;
-    (*w)[s.substr(b,  e - b)]++;
-    b = e + 1;
+class WordFreq {
+public:
+  static void get_top(int k, const std::map<string, int>& w,
+                      std::list<std::pair<string, int>>* li) {
+    if (li == NULL)
+      return;
+    li->clear();
+    for (int i=0; i<k; i++)
+      li->push_back(std::make_pair("", i-k));
+    for (auto it = w.begin(); it != w.end(); ++it)
+      insert_top(*it, li);
   }
-}
 
-void get_top(int k, const std::map<string, int>& w,
-             std::vector<std::pair<string, int>>* vec) {
-  vec->clear();
-  vec->emplace(vec->begin(), "@@@@", INT_MAX);
-  k = w.size() > k ? k : w.size();
-  for (int i=0; i<k; ++i) {
-    vec->emplace_back("@@@@", 0);
-  }
-  for (auto it=w.begin(); it!=w.end(); ++it) {
-    for (int i=k; i>=0; --i) {
-      if (vec->at(i).second >= it->second) {
-        vec->insert(vec->begin()+i+1, *it);
-        vec->pop_back();
-        break;
+  static bool insert_top(const std::pair<string, int>& w,
+                         std::list<std::pair<string, int>>* li) {
+    auto it = li->begin();
+    for (; it != li->end(); ++it) {
+      if (w.second < it->second) {
+        if (it == li->begin())
+          return false;
+        li->insert(it, w);
+        remove_least(li);
+        return true;
+      } else if (w.second == it->second) {
+        li->insert(it, w);
+        return true;
       }
     }
+    if (it == li->end()) {
+      li->push_back(w);
+      remove_least(li);
+      return true;
+    }
   }
-  vec->erase(vec->begin());
-}
+
+  static void remove_least(std::list<std::pair<string, int>>* li) {
+    int val = (li->front()).second;
+    while (val == (li->front()).second)
+      li->pop_front();
+  }
+};
+
 
 template <typename pair_list_t>
 void print_pair_list(const pair_list_t& li) {
-  for (auto it=li.begin(); it!=li.end(); ++it) {
+  for (auto it=li.rbegin(); it!=li.rend(); ++it) {
     std::cout << it->first << ": " << it->second << std::endl;
   }
 }
 
 int main(int argc, char*arg[]) {
-  if (argc < 2) {
-    std::cout << "argument to main is insufficient!" << std::endl;
-    return -1;
-  }
-  int k = atoi(arg[1]);
+  int k = argc < 1 ? 3 : atoi(arg[1]);
   string s;
-  string data;
-  while (std::cin >> data) {
-    s.append(data).append(" ");
-  }
   std::map<string, int> w;
-  split_to_words(s, &w);
-  print_pair_list(w);
+  while (std::cin >> s) {
+    w[s]++;
+  }
+  // parse word from string
+  //std::istringstream iss(str);
+  //while (iss.good()) {
+  //  string el;
+  //  iss >> el;
+  //  w[el]++;
+  //}
   std::cout << "the top " << k << " words is:" << std::endl;
-  std::vector<std::pair<string, int>> top_key;
-  get_top(k, w, &top_key);
+  std::list<std::pair<string, int>> top_key;
+  WordFreq::get_top(k, w, &top_key);
   print_pair_list(top_key);
   return 0;
 }
