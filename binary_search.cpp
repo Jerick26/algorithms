bfc0eb1bdb77b7c385efac395e53463f6d7cf6fb
Jerick26 <jerry.pin26@yahoo.com>
2017-06-17 00:56:14 +0800

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,3 +1,18 @@
+/******************************************************************************
+ *  Compilation:  g++ binary_search.cpp -std=c++0x -g
+ *  Execution:    ./main n < input.txt
+ *  Dependencies:
+ *
+ *  binary search
+ *    - find the index in an array to a indicated v
+ *  % cat imput.txt
+ *  1 2 3 3 4 4 4 5
+ *  % ./main 4 < input.txt
+ *  lower bound is: 4
+ *  upper bound is: 6
+ *
+ ******************************************************************************/
+
 #include <cstdlib>
 #include <iostream>
 #include <vector>
@@ -9,7 +24,7 @@ using std::string;
 int rank(int a[], int lo, int hi, int key) {
   while (lo <= hi) {
     int mid = (lo + hi) / 2;
-    if (key > a[mid])       lo = mid + 1;
+    if      (key > a[mid])  lo = mid + 1;
     else if (key < a[mid])  hi = mid - 1;
     else                    return mid;
   }
@@ -22,57 +37,35 @@ int rank(int a[], int lo, int hi, int key) {
 int rank_recursive(int a[], int lo, int hi, int key) {
   if (lo > hi)    return -1;
   int mid = (lo + hi) / 2;
-  if (key > a[mid])       rank(a, mid+1, hi, key);
+  if      (key > a[mid])  rank(a, mid+1, hi, key);
   else if (key < a[mid])  rank(a, lo, mid-1, key);
   else                    return mid;
-}
-
-#include <cstdlib>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
-using std::string;
-
-int binary_search(const std::vector<int>& a, int lo, int hi, int v);
-int binary_search(const std::vector<int>& a, int v);
-
-int binary_search(const std::vector<int>& a, int v) {
-  return binary_search(a, 0, a.size()-1, v);
-}
-
-int binary_search(const std::vector<int>& a, int lo, int hi, int v) {
-  if (a.size() == 0)
-    return -1;
-  while (lo <= hi) {
-    int mid = (hi + lo) / 2;
-    if (a[mid] < v) {
-      lo = mid + 1;
-    } else if (a[mid] > v) {
-      hi = mid - 1;
-    } else {
-      return mid;
-    }
-  }
-  return -1;
 }
 
 int lower_bound(const std::vector<int>& a, int v) {
   int lo = 0, hi = a.size() - 1;
   while (hi > lo) {
     int mid = (lo + hi) / 2;
-    if (a[mid] < v) {
-      lo = mid + 1;
-    } else if (a[mid] > v) {
-      hi = mid - 1;
-    } else {
-      hi = mid;
-    }
+    if      (a[mid] < v)  lo = mid + 1;
+    else if (a[mid] > v)  hi = mid - 1;
+    else                  hi = mid;
   }
   if (lo == a.size() || a[lo] != v)
     return -1;
   return lo;
+}
+
+int upper_bound(const std::vector<int>& a, int v) {
+  int lo = 0, hi = a.size() - 1;
+  while (hi > lo) {
+    int mid = (lo + hi + 1) / 2;
+    if      (a[mid] < v)  lo = mid + 1;
+    else if (a[mid] > v)  hi = mid - 1;
+    else                  lo = mid;
+  }
+  if (hi == -1 || a[hi] != v)
+    return -1;
+  return hi;
 }
 
 int main(int argc, char* argv[]) {
@@ -88,5 +81,6 @@ int main(int argc, char* argv[]) {
   std::sort(a.begin(), a.end());
   int idx = lower_bound(a, v);
   std::cout << "lower bound is: " << idx << std::endl;
+  idx = upper_bound(a, v);
+  std::cout << "upper bound is: " << idx << std::endl;
 }
-
