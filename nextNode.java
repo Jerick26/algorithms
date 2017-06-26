d79c5e66e8a9eecf37d8000b28f472369ccb26a3
Jerick26 <jerry.pin26@yahoo.com>
2017-06-26 11:47:43 +0800

    Update nextNode.java

--- nextNode.java
+++ nextNode.java
@@ -35,31 +35,3 @@ public static TreeNode findPredecessor(TreeNode node)
   }
   return y;
 }
-
-// solution b
-Node nextNode(Node node) {
-  if ( node == null ) { return null; }
-  if ( node.right != null ) {
-    node = node.right;
-    while ( node.left != null) {
-      node = node.left;
-    }
-    return node;
-  } else {
-    if ( node.parent == null) { // for root node parent  == null;
-      return null;
-    } else if ( node.parent.left == node) { // it is parent's left node.
-      return node;
-    } else {
-      // node is the right child of parent and it has no right child.
-      Node currNode = node;
-      while ( node.parent != null ){ // current node is not root
-        node = node.parent;
-        if ( node.val > currNode.val) {
-          return node;
-        }
-      }
-      return null;
-    }
-  }
-}
