881290af6332163f6bc585bc19b080b9abb93b3b
Jerick26 <jerry.pin26@yahoo.com>
2017-07-01 22:03:47 +0800

    Update lowest_common_ancestor.cpp

--- lowest_common_ancestor.cpp
+++ lowest_common_ancestor.cpp
@@ -21,23 +21,15 @@ Node* commonAncestor(Node* root, int a, int b)
  * In any binary tree
  */
 public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    if (root == null) {
-       return null;
-    }
-
+    if (root == null)  return null;
     // If the root is one of a or b, then it is the LCA
-    if (root == p || root == q) {
-        return root;
-    }
+    if (root == p || root == q)  return root;
 
     TreeNode left = lowestCommonAncestor(root.left, p, q);
     TreeNode right = lowestCommonAncestor(root.right, p, q);
-
     // If both nodes lie in left or right then their LCA is in left or right,
     // Otherwise root is their LCA
-    if (left != null && right != null) {
-        return root;
-    }
+    if (left != null && right != null)  return root;
 
     return (left != null) ? left : right;
 }
